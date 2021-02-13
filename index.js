99f212c809e7f1c6a8eae05daa66e2c26ae15f80
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2021-02-13 14:09:35 -0500

    Forgot to unset SET_URL

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// const SET_URL = "https://nicholas-miklaucic.github.io/greenglade-wisdom";
-const SET_URL = "http://0.0.0.0:8000";
+const SET_URL = "https://nicholas-miklaucic.github.io/greenglade-wisdom";
 const SPEEDS = ["Burst", "Fast", "Slow", "Unit"];
 const DEFAULT_HINT = "This is where explanatory text about what decks run this card and how to play around it will be.";
 
