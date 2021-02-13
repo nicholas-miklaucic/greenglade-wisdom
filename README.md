129846f03d2982dcb676372eb131cd7df6e837c1
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2021-02-13 00:46:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Greenglade Wisdom
+## What Is This?
+This is a tool inspired by [/u/Telebait's excellent post on the LoR
+subreddit](https://www.reddit.com/r/LegendsOfRuneterra/comments/lhr64g/i_put_together_this_cheatsheet_of_the_keycards_to/)
+showing a cheat sheet of key cards to play around. I thought it could be adapted to an interactive format to make it
+easier to read the card text and this was the result! The other inspiration for this site was [swim's
+site](https://www.swimstrim.com/runeterra/matchup-tips), which I think is out of maintenance. 
+
+This is definitely a work in progress: file an issue or a pull request if you see anything that needs fixing or
+improvement.
+## Why is [insert X card] here or not here?
+This is a list about cards you can play into by accident. The example I use on the site itself is Solari Sunforger vs.
+Radiant Guardian. They're both conditional Lifesteal units, but one is on the list and the other isn't. There are a fair
+amount of games I've played against a Demacia player who makes a weird open attack on turn 5 and, after I go "huh,
+that's weird" and block, play Radiant Guardian and make me facepalm. Sunforger doesn't have that same self-destructive
+quality: it's just a good card you see your opponent play.
+
+Similarly, Twisted Fate's Red Card is a play you can give a lot of value if you play 1-health units on turn 4 after a
+soft pass: even though you can't deny TF entirely, you can lessen the value he gives your opponent.
+
+That being said, feel free to opine on what cards you'd like to see added! I'll take it under consideration.
+
+## Why not just go to Mobalytics or Swim's site and see what meta decks are running?
+You can do that, and it will help you win games. But there's a reason Swim had a page like this: on ladder you don't
+necessarily know that your opponent is running the optimal version of any particular archetype, and you're completely on
+your own if you're playing against a homebrew. Losing to Scouts is bad enough—losing to a Kalista Back Alley Barkeep
+deck that blew you out with The Box after you played The Golden Sister and Aphelios is a lot harder on the ego. The hint
+text will also try to, well, hint at what decks might run a card or not.
+
+This list tries to strike the right balance between "how common is this" and "how bad is it if I play into this card". 
+Cards like Judgment aren't that common but are instant game winnners if allowed to be. Cards like Sharpsight can't win
+the game on the spot, but basically every Demacia deck will run it so it's worth playing against more.
+
+## What about that hint text?
+I'd like to have a little blurb for each card, but that's a lot of writing and so it's still a work in progress. The
+hints I currently have are stored in the hints.json file. Feel free to submit a pull request to add information!
---
+++ index.js
@@ -0,0 +1,181 @@
+const SET_URL = "http://0.0.0.0:8000";
+const SPEEDS = ["Burst", "Fast", "Slow", "Unit"];
+
+const CARD_NAMES = [
+    // Demacia
+    "Ranger's Resolve",
+    "Sharpsight",
+    "Riposte",
+    "Single Combat",
+    "Concerted Strike",
+    "Judgment",
+    "Relentless Pursuit",
+    "Radiant Guardian",
+    // Frejlord
+    "Brittle Steel",
+    "Troll Chant",
+    "Flash Freeze",
+    "Harsh Winds",
+    "Avalanche",
+    "Icequake ", // Riot pls why
+    // Ionia
+    "Twin Disciplines",
+    "Spirit's Refuge",
+    "Flurry of Fists",
+    "Nopeify!",
+    "Deny",
+    "Concussive Palm",
+    // P&Z
+    "Suit Up!",
+    "Mystic Shot",
+    "Get Excited!",
+    "Statikk Shock",
+    "Thermogenic Beam",
+    "Tri-beam Improbulator",
+    "Aftershock",
+    // Noxus
+    "Transfusion",
+    "Brothers' Bond",
+    "Ravenous Flock",
+    "Culling Strike",
+    "Scorched Earth",
+    "Noxian Fervor",
+    "Decimate",
+    "Arachnoid Sentry",
+    // SI
+    "Mark of the Isles",
+    "Vile Feast",
+    "Black Spear",
+    "The Box",
+    "Withering Wail",
+    "Grasp of the Undying",
+    "Vengeance",
+    "The Harrowing",
+    "The Ruination",
+    "Frenzied Skitterer",
+    // Bilgewater
+    "Jettison",
+    "Make it Rain",
+    "Mind Meld",
+    "Twisted Fate",
+    "Devourer of the Depths",
+    // Targon
+    "Pale Cascade",
+    "Hush",
+    "Bastion",
+    "Moonlight Affliction",
+    "Gravitum",
+    "Crescent Strike",
+    "Meteor Shower",
+    "Falling Comet",
+    "Supernova",
+    "Cosmic Rays",
+    "The Serpent",
+    "The Golden Sister",
+];
+
+function findCardName(cards, name) {
+    for (let card of cards) {
+        if (card.name === name) {
+            return card;
+        }
+    }
+    throw "Card name '" + name + "' is not defined";
+}
+
+function cardToHTML(card) {
+    return `
+<figure class="is-4by3 card-img">
+  <img src="${card.assets[0].gameAbsolutePath}">
+</figure>
+`
+}
+
+function cardSpeed(card) {
+    if (card.type == "Spell") {
+        return card.spellSpeed;
+    } else {
+        return "Unit";
+    }
+}
+
+function regionButton(reg) {
+    return `
+<label class="checkbox">
+<input type="checkbox" id="${reg.nameRef}-check" name="${reg.nameRef}-check" class="reg-selector" checked>
+<img class="region-icon" src="${reg.iconAbsolutePath}">
+</figure> ${reg.name}
+</label>
+`
+}
+
+let mana = document.getElementById("mana");
+let manaNum = document.getElementById("manaNum");
+mana.addEventListener("change", function() {
+    manaNum.value = mana.value;
+});
+
+manaNum.addEventListener("change", function() {
+    mana.value = manaNum.value;
+});
+
+
+d3.json(SET_URL + "/static/global/en_us/data/globals-en_us.json", function(globals) {
+    d3.select("#regions-select")
+      .selectAll(".reg-col")
+      .data(globals.regions.sort((a, b) => d3.ascending(a.name, b.name)))
+      .enter()
+      .append("div")
+      .attr("class", "column is-6-tablet is-3-desktop is-3-widescreen is-3-fullhd reg-col")
+      .html(regionButton);
+    d3.json(SET_URL + "/static/set1/en_us/data/set1-en_us.json", function(set1) {
+        d3.json(SET_URL + "/static/set2/en_us/data/set2-en_us.json", function(set2) {
+            d3.json(SET_URL + "/static/set3/en_us/data/set3-en_us.json", function(set3) {
+                d3.json(SET_URL + "/static/hints.json", function(hints) {
+                    console.log(hints);
+                    const ALL_CARDS = set1.concat(set2.concat(set3));
+                    let cards = d3.map(CARD_NAMES, name => findCardName(ALL_CARDS, name));
+
+                    function setHint(card, hints) {
+                        let hintText = hints[card.name];
+                        if (hintText === undefined) {
+                            hintText = hints["default"];
+                        }
+                        document.getElementById("hint-area").textContent = hintText;
+                    }
+
+
+                    function updateCards(cards) {
+                        console.log("Updating...")
+                        for (let speed of SPEEDS) {
+                            let cardObjs = d3.select("#cards-" + speed.toLowerCase())
+                                             .selectAll("div.column")
+                                             .data(d3.filter(cards, card => cardSpeed(card) === speed), d => d);
+                            cardObjs.enter()
+                                    .append("div")
+                                    .attr("class", "column is-12-desktop is-6-widescreen is-6-fullhd")
+                                    .attr("id", card => card.cardCode)
+                                    .html(cardToHTML)
+                                    .on("mouseover", card => setHint(card, hints));
+                            cardObjs.exit()
+                                    .remove();
+
+                        }
+                    }
+                    function update() {
+                        console.log("Hi");
+                        newData = d3.filter(cards, card => d3.select("#" + card.regionRef + "-check").property("checked"));
+                        newData = d3.filter(newData, card => card.cost <= d3.select("#mana").property("value"));
+                        newData = newData.sort((a, b) => d3.ascending(a.cost, b.cost));
+                        updateCards(newData);
+                    };
+                    d3.selectAll(".reg-selector")
+                      .on("change", update);
+                    d3.select("#mana").on("change", update);
+                    update();
+                    setHint({"name": "default"}, hints);
+                });
+            });
+        });
+    });
+});
